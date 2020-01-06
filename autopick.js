fb2a4200776206653192f4d509925c5187b15114
ahsanatha <ahsan.athallah@gmail.com>
2020-01-06 14:11:18 +0700

    Update autopick.js

--- autopick.js
+++ autopick.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         Auto pick matkupil
+// @name         autopick.
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  auto pick mk pilihanmu if available,else it will automatically refresh
-// @author       raisoturu
+// @author       tsr
 // @match        https://igracias.telkomuniversity.ac.id/registration/*
 // @grant        none
 // @require      https://igracias.telkomuniversity.ac.id/javascripts/ui/jquery.ui.tabs.min.js
@@ -14,30 +14,31 @@
   "use strict";
   $("#tabs").tabs("select", 3); //select tab first
   const delay = 10000; //delay
-  // const subjectCode = "CCH4A3"; //testing
-  const subjectCode = "CSH4O3"; //NLP
-  // const subjectCode = 'CSH4G3' //DATA MINING
   setTimeout(() => {
-    console.log(subjectCode);
     window.scrollTo(0, screen.height + 700);
-  }, 3000);
-  setTimeout(() => {
+  }, delay);
+
+  function dodeting(subjectCode) {
+    console.log('lagi ngeliat',subjectCode);
     const matkul = $(`input[subjectcode='${subjectCode}']`);
-    console.log(matkul);
     if (matkul.length) {
       const x = Math.floor(Math.random() * matkul.length); //generate random number
       const cls = matkul
         .parent()
         .parent()
         .children()[2];
-      console.log(cls.textContent);
-      console.log(cls.textContent != "ICM-40-INT");
-      if (cls.textContent != "ICM-40-INT") {
+      console.log("Kelas", cls.textContent);
+      if (
+        cls.textContent != "ICM-40-INT" &&
+        cls.textContent != "SIDE-40-INT" &&
+        cls.textContent != "MCE-40-INT" &&
+        cls.textContent != "TELE-40-INT"
+      ) {
         //gaboleh ngambil inter ya guys
         matkul[x].checked = true;
       } else {
         console.log("Inter gan");
-        location.reload();
+        // location.reload();
       }
       if (matkul[x].checked == true) {
         console.log("mantap");
@@ -45,7 +46,12 @@
       }
     } else {
       console.log("gadapet gan");
-      location.reload();
+      // location.reload();
     }
+  }
+  setTimeout(() => {
+    var lookupmk = ["CSH4X3","CSH4G3","CSH4F3"]; // masukan mk yg anda mau disini
+    lookupmk.forEach(dodeting);
+    location.reload();
   }, delay);
 })();
